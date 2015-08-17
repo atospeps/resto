df61893144928d9c5be65731878dddb3f3d45365
iraichsp <ivan.raichs@atos.net>
2015-08-17 16:08:12 +0200

    Mise à jour du model Sentinel1

--- include/resto/Models/RestoModel_sentinel1.php
+++ include/resto/Models/RestoModel_sentinel1.php
@@ -41,32 +41,8 @@
 /**
  * RESTo Sentinel-1 model 
  * 
- * Input metadata is an XML file with the following structure 
+ * Input metadata is an XML file 
  * 
- <product>
-  <adsHeader>
-    <missionId>S1A</missionId>
-    <productType>GRD</productType>
-    <polarisation>VV</polarisation>
-    <mode>IW</mode>
-    <swath>IW</swath>
-    <startTime>2014-10-03T18:47:39.842715</startTime>
-    <stopTime>2014-10-03T18:48:08.834276</stopTime>
-    <absoluteOrbitNumber>2669</absoluteOrbitNumber>
-    <missionDataTakeId>12181</missionDataTakeId>
-    <imageNumber>001</imageNumber>
-  </adsHeader>
-  (...)
-  <geolocationGrid>
-    <geolocationGridPointList count="231">
-      <geolocationGridPoint>
-        <azimuthTime>2014-10-03T18:47:39.842455</azimuthTime>
-        <slantRangeTime>5.364633780973990e-03</slantRangeTime>
-        <line>0</line>
-        <pixel>0</pixel>
-        <latitude>6.588778439216060e+01</latitude>
-        <longitude>1.785002983064243e+02</longitude>
-    (...)
  */
 class RestoModel_sentinel1 extends RestoModel {
     
@@ -109,7 +85,81 @@ class RestoModel_sentinel1 extends RestoModel {
     /**
      * Create JSON feature from xml string
      * 
+     * @param {String} $xml : $xml string
+     */
+    private function parse($xml) {
+        
+        $dom = new DOMDocument();
+        $dom->loadXML(rawurldecode($xml));
+        
+        //adsHeader is a tag only found in the old xml version
+        $verifyVersion = $dom->getElementsByTagName('adsHeader');
+        if($verifyVersion->length == 0){
+            // We parse the file with the new version
+            return $this->parseNew($dom);
+        }else{
+            // We parse the file with the old version
+            return $this->parseOld($dom);
+        }
+    }
+    
+    /**
+     * Create JSON feature from new resource xml string
+     *
      * <product>
+    <title>S1A_IW_OCN__2SDV_20150727T044706_20150727T044731_006992_0097D1_F6DA</title>
+    <resourceSize>6317404</resourceSize>
+    <startTime>2015-07-27T04:47:06.611</startTime>
+    <stopTime>2015-07-27T04:47:31.061</stopTime>
+    <productType>OCN</productType>
+    <missionId>S1A</missionId>
+    <processingLevel>2</processingLevel>
+    <mode>IW</mode>
+    <absoluteOrbitNumber>6992</absoluteOrbitNumber>
+    <orbitDirection>ASCENDING</orbitDirection>
+    <swath>IW</swath>
+    <polarisation>VV VH</polarisation>
+    <missiontakeid>38865</missiontakeid>
+    <footprint>POLYGON ((-161.306549 21.163258,-158.915909 21.585093,-158.623169 20.077986,-160.989746 19.652864,-161.306549 21.163258))</footprint>
+    </product>
+     *
+     * @param {DOMDocument} $dom : $dom DOMDocument
+     */
+    private function parseNew($dom){
+        /*
+         * Initialize feature
+         */
+        $feature = array(
+                'type' => 'Feature',
+                /*'geometry' => array(
+                        'type' => 'Polygon',
+                        'coordinates' => array($polygon)
+                ),*/
+                'properties' => array(
+                        'productIdentifier' => $dom->getElementsByTagName('title')->item(0)->nodeValue,
+                        'title' => $dom->getElementsByTagName('title')->item(0)->nodeValue,
+                        'resourceSize' => $dom->getElementsByTagName('resourceSize')->item(0)->nodeValue,
+                        'authority' => 'ESA',
+                        'startDate' => $dom->getElementsByTagName('startTime')->item(0)->nodeValue,
+                        'completionDate' => $dom->getElementsByTagName('stopTime')->item(0)->nodeValue,
+                        'productType' => $dom->getElementsByTagName('productType')->item(0)->nodeValue,
+                        'processingLevel' => 'LEVEL1',
+                        'platform' => $dom->getElementsByTagName('missionId')->item(0)->nodeValue,
+                        'sensorMode' => $dom->getElementsByTagName('mode')->item(0)->nodeValue,
+                        'orbitNumber' => $dom->getElementsByTagName('absoluteOrbitNumber')->item(0)->nodeValue,
+                        'orbitDirection' => $dom->getElementsByTagName('orbitDirection')->item(0)->nodeValue,
+                        'quicklook'=> $this->getLocation($dom),
+                        'cloudCover' => 0
+                )
+        );
+
+        return $feature;
+    }
+    
+    /**
+     * Create JSON feature from old resource xml string
+     *
+ <product>
   <adsHeader>
     <missionId>S1A</missionId>
     <productType>GRD</productType>
@@ -133,32 +183,28 @@ class RestoModel_sentinel1 extends RestoModel {
         <latitude>6.588778439216060e+01</latitude>
         <longitude>1.785002983064243e+02</longitude>
     (...)
-     * 
-     * @param {String} $xml : $xml string
+     *
+     * @param {DOMDocument} $dom : $dom DOMDocument
      */
-    private function parse($xml) {
-        
-        $dom = new DOMDocument();
-        $dom->loadXML(rawurldecode($xml));
-        
+    private function parseOld($dom){
         $geolocationGridPoint = $dom->getElementsByTagName('geolocationGridPoint');
         $ul = array();
         $ur = array();
         $ll = array();
         $lr = array();
         $lineMax = 0;
-	$lineMin = 0;
-	$lineMinStatus = 0;
+        $lineMin = 0;
+        $lineMinStatus = 0;
         $pixelMax = 0;
-        for ($i = 0, $ii = $geolocationGridPoint->length; $i < $ii; $i++) { 
+        for ($i = 0, $ii = $geolocationGridPoint->length; $i < $ii; $i++) {
             $line = (integer) $geolocationGridPoint->item($i)->getElementsByTagName('line')->item(0)->nodeValue;
             $pixel = (integer) $geolocationGridPoint->item($i)->getElementsByTagName('pixel')->item(0)->nodeValue;
-            $coordinates = array($geolocationGridPoint->item($i)->getElementsByTagName('longitude')->item(0)->nodeValue, $geolocationGridPoint->item($i)->getElementsByTagName('latitude')->item(0)->nodeValue);     
-	    if ($lineMinStatus == 0)
-		{
-		  $lineMinStatus = 1;
-		  $lineMin=$line;
-		}
+            $coordinates = array($geolocationGridPoint->item($i)->getElementsByTagName('longitude')->item(0)->nodeValue, $geolocationGridPoint->item($i)->getElementsByTagName('latitude')->item(0)->nodeValue);
+            if ($lineMinStatus == 0)
+            {
+                $lineMinStatus = 1;
+                $lineMin=$line;
+            }
             if ($line === $lineMin) {
                 if ($pixel === 0) {
                     $ul = $coordinates;
@@ -178,36 +224,36 @@ class RestoModel_sentinel1 extends RestoModel {
                     $lr = $coordinates;
                 }
             }
-        } 
+        }
         $polygon = array($ul, $ur, $lr, $ll, $ul);
         /*
          * Initialize feature
-         */
+        */
         $feature = array(
-            'type' => 'Feature',
-            'geometry' => array(
-                'type' => 'Polygon',
-                'coordinates' => array($polygon)
-            ),
-            'properties' => array(
-                'productIdentifier' => $dom->getElementsByTagName('title')->item(0)->nodeValue,
-                'title' => $dom->getElementsByTagName('title')->item(0)->nodeValue,
-                'resourceSize' => $dom->getElementsByTagName('resourceSize')->item(0)->nodeValue,
-		        'authority' => 'ESA',
-                'startDate' => $dom->getElementsByTagName('startTime')->item(0)->nodeValue,
-                'completionDate' => $dom->getElementsByTagName('stopTime')->item(0)->nodeValue,
-                'productType' => $dom->getElementsByTagName('productType')->item(0)->nodeValue,
-                'processingLevel' => 'LEVEL1',
-                'platform' => $dom->getElementsByTagName('missionId')->item(0)->nodeValue,
-                'sensorMode' => $dom->getElementsByTagName('mode')->item(0)->nodeValue,
-                'orbitNumber' => $dom->getElementsByTagName('absoluteOrbitNumber')->item(0)->nodeValue,
-            	'quicklook'=> $this->getLocation($dom),
-                'cloudCover' => 0
-	    )
+                'type' => 'Feature',
+                'geometry' => array(
+                        'type' => 'Polygon',
+                        'coordinates' => array($polygon)
+                ),
+                'properties' => array(
+                        'productIdentifier' => $dom->getElementsByTagName('title')->item(0)->nodeValue,
+                        'title' => $dom->getElementsByTagName('title')->item(0)->nodeValue,
+                        'resourceSize' => $dom->getElementsByTagName('resourceSize')->item(0)->nodeValue,
+                        'authority' => 'ESA',
+                        'startDate' => $dom->getElementsByTagName('startTime')->item(0)->nodeValue,
+                        'completionDate' => $dom->getElementsByTagName('stopTime')->item(0)->nodeValue,
+                        'productType' => $dom->getElementsByTagName('productType')->item(0)->nodeValue,
+                        'processingLevel' => 'LEVEL1',
+                        'platform' => $dom->getElementsByTagName('missionId')->item(0)->nodeValue,
+                        'sensorMode' => $dom->getElementsByTagName('mode')->item(0)->nodeValue,
+                        'orbitNumber' => $dom->getElementsByTagName('absoluteOrbitNumber')->item(0)->nodeValue,
+                        'orbitDirection' => $dom->getElementsByTagName('pass')->item(0)->nodeValue,
+                        'quicklook'=> $this->getLocation($dom),
+                        'cloudCover' => 0
+                )
         );
-
-        return $feature;
         
+        return $feature;
     }
 
     function getLocation($dom) {

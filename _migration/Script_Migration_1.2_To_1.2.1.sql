c325c1ecf7b7cebecf9adc6232cb03b515516369
iraichsp <ivan.raichs@atos.net>
2015-08-17 17:16:54 +0200

    Rajouter colonne orbitDirection pour la collection S1

---
+++ _migration/Script_Migration_1.2_To_1.2.1.sql
@@ -0,0 +1,28 @@
+/* Script de migration de la base de données Resto de la version 1.2 vers 1.2.1 */
+/* Rajouter la colonne orbitDirection dans la table features de la collection s1 et de Resto  */
+/* Change the default value of instantdownaloadlimit and weeklydownloadlimit (value in MegaOctet) */
+CREATE OR REPLACE function f_add_col(
+   _tbl regclass, _col  text, _type regtype, OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN
+
+IF EXISTS (
+   SELECT 1 FROM pg_attribute
+   WHERE  attrelid = _tbl
+   AND    attname = _col
+   AND    NOT attisdropped) THEN
+   success := FALSE;
+
+ELSE
+   EXECUTE '
+   ALTER TABLE ' || _tbl || ' ADD COLUMN ' || quote_ident(_col) || ' ' || _type;
+   success := TRUE;
+END IF;
+
+END
+$func$;
+
+SELECT f_add_col('_s1.features', 'orbitDirection', 'text');
+
+SELECT f_add_col('resto.features', 'orbitDirection', 'text');

ab3079fbe2faecd70dbea32c31474ecb31d674e0
Sebastien Rabouin <sebastien.rabouin@atos.net>
2015-12-07 14:16:01 +0100

    Ajout du champ orbitDirection pour S2

--- _migration/Script_Migration_2.2.4_To_2.3.sql
+++ _migration/Script_Migration_2.2.4_To_2.3.sql
@@ -1,4 +1,28 @@
 /* Script de migration de la base de données Resto de la version 2.2.4 vers 2.2.5 */
+CREATE OR REPLACE function f_add_col(
+   _tbl regclass, _col  text, _type regtype, OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN
+
+IF EXISTS (
+   SELECT 1 FROM pg_attribute
+   WHERE  attrelid = _tbl
+   AND    attname = _col
+   AND    NOT attisdropped) THEN
+   success := FALSE;
+
+ELSE
+   EXECUTE '
+   ALTER TABLE ' || _tbl || ' ADD COLUMN ' || _col || ' ' || _type;
+   success := TRUE;
+END IF;
+
+END
+$func$;
+
+SELECT f_add_col('_s2.features', 'orbitDirection', 'text');
+
 UPDATE resto.collections SET mapping='{"parentIdentifier": "urn:ogc:def:EOP:ESA::SENTINEL-1:", "quicklook" : "https://peps.cnes.fr/quicklook/{:quicklook:}_quicklook.jpg", "resource" : "/hpss/peps/data/{:quicklook:}.zip", "resourceMimeType": "application/zip", "wms" : "https://peps.cnes.fr/cgi-bin/mapserver?map=WMS_S1&data={:quicklook:}"}' WHERE collection='S1';
 
 UPDATE resto.collections SET mapping='{"parentIdentifier": "urn:ogc:def:EOP:ESA::SENTINEL-2:", "quicklook" : "https://peps.cnes.fr/quicklook/{:quicklook:}_quicklook.jpg", "resource" : "/hpss/peps/data/{:quicklook:}.zip", "resourceMimeType": "application/zip"}' WHERE collection='S2';

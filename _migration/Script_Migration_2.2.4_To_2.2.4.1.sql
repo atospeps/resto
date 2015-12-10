8764aa5f5e45fb1022d5e0164aa4c75454799f46
Sebastien Rabouin <sebastien.rabouin@atos.net>
2015-12-10 15:24:13 +0100

    Ajout script de migration ajoutant le champ orbitDirection pour S2

---
+++ _migration/Script_Migration_2.2.4_To_2.2.4.1.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE function f_add_col(
+   _tbl regclass, _col  text, _type regtype, OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN
+
+IF EXISTS (
+   SELECT 1 FROM pg_attribute
+   WHERE  attrelid = _tbl
+   AND    attname = _col
+   AND    NOT attisdropped) THEN
+   success := FALSE;
+
+ELSE
+   EXECUTE '
+   ALTER TABLE ' || _tbl || ' ADD COLUMN ' || _col || ' ' || _type;
+   success := TRUE;
+END IF;
+
+END
+$func$;
+
+SELECT f_add_col('_s2.features', 'orbitDirection', 'text');
+

bf331aadd0af782957987be9d2611646061a799f
delmaale <driss.elmaalem@atos.net>
2015-12-15 17:09:34 +0100

    Major update - check authentication - Update routes

--- include/resto/Modules/Alerts.php
+++ include/resto/Modules/Alerts.php
@@ -2,26 +2,28 @@
 
 /**
  *
-* Alerts module
-*
-* Manage alerts
-* 
-*    |          Resource                                                |     Description
-*    |__________________________________________________________________|______________________________________
-*    |  GET     alerts                                                  |  List all subscriptions
-*    |  POST    alerts                                                  |  Create or edit a subscription
-*    |  POST    alerts/clear                                            |  Delete a subscription
-*    |  GET     alerts/execute                                          |  Execute the subscriptions
-* 
-*/
-
+ * @author Atos 
+ * Alerts module
+ *
+ * Manage alerts
+ *    
+ *    | Resource                                                             | Description
+ *    |___________________________________________________________________________________________________________
+ *    |
+ *    | HTTP/GET  /alerts                                                    | List all subscriptions
+ *    | HTTP/POST /alerts                                                    | Adds alert
+ *    | HTTP/POST /alerts/{alertid}                                          | Edit alert
+ *    | HTTP/POST /alerts/{alertid}/clear                                    | Deletes alert
+ *    | HTTP/POST /alerts/execute                                            | Execute the subscriptions
+ * 
+ */
 class Alerts extends RestoModule {
     
     /*
      * Resto context
      */
     public $context;
-
+    
     /*
      * Current user (only set for administration on a single user)
      */
@@ -31,12 +33,12 @@ class Alerts extends RestoModule {
      * segments
      */
     public $segments;
-
+    
     /*
      * Database handler
      */
     private $dbh;
-
+    
     /**
      * Constructor
      *
@@ -45,6 +47,7 @@ class Alerts extends RestoModule {
      */
     public function __construct($context, $user) {
         parent::__construct($context, $user);
+        
         // Set user
         $this->user = $user;
         
@@ -54,43 +57,37 @@ class Alerts extends RestoModule {
         // Database handler
         $this->dbh = $this->getDatabaseHandler();
     }
-
+    
     /**
      * Run module - this function should be called by Resto.php
      *
      * @param array $elements : route elements
      * @param array $data : POST or PUT parameters
-     *
+     *       
      * @return string : result from run process in the $context->outputFormat
      */
     public function run($segments, $data = array()) {
-
+        
         /*
          * Only autenticated user.
          */
-        if ($this->user->profile['userid'] == -1) {            
+        if ($this->user->profile['userid'] == -1) {
             RestoLogUtil::httpError(401);
         }
-        
-        /*
-         * Only GET method and POST are accepted
-         */
-        if ($this->context->method !== 'GET' && $this->context->method !== 'POST') {
-            RestoLogUtil::httpError(404);
-        }
-        
+
         $this->segments = $segments;
         $method = $this->context->method;
 
         /*
          * Switch on HTTP methods
+         * Only GET method and POST are accepted
          */
         switch ($method) {
-            case 'GET':
+            case 'GET' :
                 return $this->processGET();
-            case 'POST':
+            case 'POST' :
                 return $this->processPOST($data);
-            default:
+            default :
                 RestoLogUtil::httpError(404);
         }
     }
@@ -101,88 +98,119 @@ class Alerts extends RestoModule {
      * @throws Exception
      */
     private function processGET() {
-        if (isset($this->segments[0]) && $this->segments[0] = 'execute') {
-            // Verify the allowed hosts for the execution.
-            if(isset($this->context->modules["Alerts"]["allowed_execute_hosts"]) && !empty($this->context->modules["Alerts"]["allowed_execute_hosts"])){
-                if (!in_array($_SERVER['REMOTE_ADDR'],$this->context->modules["Alerts"]["allowed_execute_hosts"])) {
-                    RestoLogUtil::httpError(403);
-                }
-            }
-          // Execute  
-          $this->alertExecute(); 
-        } else if (!isset($this->segments[0])) {
-            // Verify user is set
-            if (isset($this->user->profile['email'])) {
-                $alerts = pg_query($this->dbh, 'SELECT * from usermanagement.alerts WHERE email = \'' . pg_escape_string($this->user->profile['email']) . '\'');
-                $result = array ();
-                while ($row = pg_fetch_assoc($alerts)) {
-                    $result[] = $row;
-                }
-                return $result;
-            } else {
-                RestoLogUtil::httpError(403);
-            }
-        } else {
+        /*
+         * alerts
+         */
+        if (!isset($this->segments[0])) {
+            return $this->getUserAlerts($this->user->profile['email']);
+        }         
+        /*
+         * Unknown route
+         */
+        else {
             RestoLogUtil::httpError(404);
         }
+    }
+    
+    /**
+     * Returns user alerts.
+     *
+     * @param string $identifier : email
+     */
+    private function getUserAlerts() {
+        $identifier = $this->user->profile['email'];
+        $query = 'SELECT * from usermanagement.alerts WHERE email = \'' . pg_escape_string($identifier) . '\'';
+        $alerts = pg_query($this->dbh, $query);
+        $result = array ();
+        while ($row = pg_fetch_assoc($alerts)) {
+            $row['criterias'] = json_decode($row['criterias']);
+            $result[] = $row;
+        }
+        
+        return RestoLogUtil::success('Searches list for user ' . $this->user->profile['userid'], array (
+                'items' => $result 
+        ));
     }
     
     /**
      * Process on HTTP method POST on /alerts and alerts/clear
      *
+     *
+     * | HTTP/POST /alerts | Adds alert
+     * | HTTP/POST /alerts/{alertid} | Edits alert
+     * | HTTP/POST /alerts/{alertid}/clear | Deletes alert
+     * | HTTP/POST /alerts/execute | Execute the subscriptions
+     *
      * @throws Exception
      */
     private function processPOST($data) {
+
         /*
-         * Get the operation to proceed
-         * HTTP/GET alert
+         * alerts
          */
-        if (!isset($this->segments[0]) && !isset($data['aid'])) {
-            // If there is no identifier, an alert is created
+        if (!isset($this->segments[0])) {
             return $this->createAlert($data);
         }
-        // HTTP/GET alert?aid={aid}
-        else if (!isset($this->segments[0]) && isset($data['aid'])) {
-            // If there is an aid, we are editing an  existing alert
-            return $this->editAlert($data);
-        } 
-        // HTTP/GET alert/clear
-        else if ($this->segments[0] == 'clear') {
-            // With the segment clear, we delete an alert
-            return $this->deleteAlert($data);
+        /*
+         * alerts/execute
+         * alerts/{alertid}
+         */
+        else if (isset($this->segments[0]) && !isset($this->segments[1])) {
+            switch ($this->segments[0]) {
+                case 'execute':
+                    /*
+                     * Execute
+                     */
+                    return $this->alertExecute();
+                default:
+                    /*
+                     * Edits alert
+                     */
+                    return $this->editAlert($this->segments[1], $data);
+            }
         }
-         else {
+        /*
+         * alerts/{alertid}/clear
+         */
+        else if (isset($this->segments[0]) && !isset($this->segments[2]) && ($this->segments[1] == 'clear') ) {
+            return $this->deleteAlert($this->segments[0]);
+        }
+        /*
+         * Unknown route
+         */
+        else {
             RestoLogUtil::httpError(404);
         }
     }
     
     /**
-     * We create an alert
+     * Store alert.
      *
      * @throws Exception
      */
     private function createAlert($data) {
-
         try {
             /*
              * Gets alert properties.
              */
-            $now = date("Y-m-d H:i:s");
+            $now = '\'' . date("Y-m-d H:i:s") . '\'';
+            $identifier = '\'' . pg_escape_string($this->user->profile['email']) . '\'';
             $title = isset($data['title']) ? '\'' . pg_escape_string($data['title']) . '\'' : 'NULL';
             $expiration = isset($data['expiration']) ? '\'' . pg_escape_string($data['expiration']) . '\'' : 'NULL';
             $period = (isset($data['period']) && is_numeric($data['period'])) ? '\'' . $data['period'] . '\'' : 'NULL';
-            $hasSubscribe = (isset($data['hasSubscribe']) && $data['hasSubscribe'] == true) ? '\'' . $data['hasSubscribe'] . '\'' : 'NULL';
+            $hasSubscribe = (isset($data['hasSubscribe']) && $data['hasSubscribe'] == true) ? 1 : 0;
+            $criterias = '\'' . json_encode($data['criterias']) . '\'';
             
-            $values = array(
-                    '\'' . pg_escape_string($this->user->profile['email']) . '\'', 
-                    $title, 
-                    '\'' . $now . '\'', 
-                    $expiration, 
-                    '\'' . $now . '\'', 
+            $values = array (
+                    $identifier,
+                    $title,
+                    $now,
+                    $expiration,
+                    $now,
                     $period,
-                    '\'' . json_encode($data['criterias']) . '\'',
-                    $hasSubscribe
-                    );
+                    $criterias,
+                    $hasSubscribe 
+            );
             /*
              * Stores alert.
              */
@@ -190,54 +218,62 @@ class Alerts extends RestoModule {
                     VALUES (' . join(',', $values) . ')';
             
             $alerts = pg_query($this->dbh, $query);
-            return RestoLogUtil::success('');
-            
+
+            return RestoLogUtil::success('Search context created for user ' . $this->user->profile['userid']);
         } catch (Exception $e) {
             RestoLogUtil::httpError($e->getCode(), $e->getMessage());
         }
     }
-    
+
     /**
-     * We edit an alert
+     * Edit alert.
      *
      * @throws Exception
      */
-    private function editAlert($data) {
-        // Edit an alert using the alert id
-        if (isset($data['aid'])) {
-            try {
-                $alerts = pg_query($this->dbh, 'UPDATE usermanagement.alerts SET 
-                        email=\'' . pg_escape_string($data['email']) . '\', title=\'' . pg_escape_string($data['title']) 
-                        . '\', expiration=\'' . pg_escape_string($data['expiration']) . '\', period=\'' . pg_escape_string($data['period']) .
-                        '\', criterias=\'' . pg_escape_string($data['criterias']) . '\' WHERE aid=\'' . pg_escape_string($data['aid']) . '\'');
-                return array ('status' => 'success', 'message' => 'success');
-            } catch (Exception $e) {
-                RestoLogUtil::httpError($e->getCode(), $e->getMessage());
-            }
-        } else {
-            RestoLogUtil::httpError(404);
+    private function editAlert($alertid, $data) {
+        try {
+
+            $identifier = $this->user->profile['email'];
+            
+            
+            
+            $title = isset($data['title']) ? '\'' . pg_escape_string($data['title']) . '\'' : 'NULL';
+            $expiration = isset($data['expiration']) ? '\'' . pg_escape_string($data['expiration']) . '\'' : 'NULL';
+            $period = (isset($data['period']) && is_numeric($data['period'])) ? '\'' . $data['period'] . '\'' : 'NULL';
+            $hasSubscribe = (isset($data['hasSubscribe']) && $data['hasSubscribe'] == true) ? 1 : 0;
+            $criterias = '\'' . json_encode($data['criterias']) . '\'';
+            
+            
+            $query = 'UPDATE usermanagement.alerts '
+                    . 'title=\'' . pg_escape_string($data['title']) . '\', expiration=\'' . pg_escape_string($data['expiration']) . '\', period=\'' . pg_escape_string($data['period']) . '\', criterias=\'' . pg_escape_string($data['criterias']) . '\' ' 
+                    . 'WHERE aid = \'' . pg_escape_string($alertid) . '\' AND email = \'' . pg_escape_string($identifier) . '\'';
+            $alerts = pg_query($this->dbh, $query);
+
+            return RestoLogUtil::success('Search context ' . $alertid . ' updated');
+        } catch (Exception $e) {
+            RestoLogUtil::httpError($e->getCode(), $e->getMessage());
         }
     }
-    
+
     /**
-     * We delete an alert
+     * Delete alert.
      *
      * @throws Exception
      */
-    private function deleteAlert($data) {
-        // Delete an alert using the alert id
-        if (isset($data['aid'])) {
-            try {
-                $alerts = pg_query($this->dbh, 'DELETE FROM usermanagement.alerts WHERE aid = \'' . pg_escape_string($data['aid']) . '\'');
-                return array ('status' => 'success', 'message' => 'success');
-            } catch (Exception $e) {
-                RestoLogUtil::httpError($e->getCode(), $e->getMessage());
-            }
-        } else {
-            RestoLogUtil::httpError(404);
+    private function deleteAlert($alertid) {
+        try {
+            $identifier = $this->user->profile['email'];
+            $query = 'DELETE FROM usermanagement.alerts WHERE aid = \'' . pg_escape_string($alertid) . '\' AND email = \'' . pg_escape_string($identifier) . '\'';
+            
+            print_r($query);
+            $alerts = pg_query($this->dbh, $query);
+            
+            return RestoLogUtil::success('Search context ' . $alertid . ' deleted');
+        } catch (Exception $e) {
+            RestoLogUtil::httpError($e->getCode(), $e->getMessage());
         }
     }
-    
+
     /**
      * We execute the alerts.
      * We send the mails to the users
@@ -276,28 +312,26 @@ class Alerts extends RestoModule {
                     $params['content'] = $content;
                     
                     // We send the mail
-                    if($this->sendAttachedMeta4Mail($params)){
-                        //After sending the mail we update the database with the new last_dispatch
+                    if ($this->sendAttachedMeta4Mail($params)) {
+                        // After sending the mail we update the database with the new last_dispatch
                         pg_query($this->dbh, "UPDATE usermanagement.alerts SET last_dispatch='" . date("Y-m-d\TH:00:00", time()) . "' WHERE aid=" . $row["aid"]);
                     }
-                    
                 }
             }
         }
     }
     
     /**
-     * We pass the products returned in the opensearch and we convert them into 
-     * a meta4 links ready to be downloaded  
-     * 
+     * We pass the products returned in the opensearch and we convert them into
+     * a meta4 links ready to be downloaded
+     *
      * @param array $items : all the products returned byt the open search
      */
     private function alertsToMETA4($items, $email) {
-        
         $meta4 = new RestoMetalink($this->context);
         
-        //One metalink file per item - if user has rights to download file
-        foreach ($items as $item) {          
+        // One metalink file per item - if user has rights to download file
+        foreach ($items as $item) {
             // Invalid item
             if (!isset($item['properties']) || !isset($item['properties']['services']) || !isset($item['properties']['services']['download'])) {
                 continue;
@@ -326,14 +360,13 @@ class Alerts extends RestoModule {
     }
     
     /**
-     * 
-     * 
+     *
      * @param string $action
      * @param string $collectionName
      * @param string $featureIdentifier
      * @return If user has the rights to download a product
      */
-    private function canAlertsDownload($action, $collectionName = null, $featureIdentifier = null, $email){
+    private function canAlertsDownload($action, $collectionName = null, $featureIdentifier = null, $email) {
         // We need to establish the user group
         $result = pg_query($this->dbh, "SELECT groupname FROM usermanagement.users WHERE email='" . $email . "'");
         $group = pg_fetch_assoc($result);
@@ -426,18 +459,18 @@ class Alerts extends RestoModule {
             $arguments = array ();
             foreach ($criterias as $key => $value) {
                 // We don't insert the collection as an argument
-                if($key != 'collection'){    
-                $arguments[] = $key . '=' . $value;
+                if ($key != 'collection') {
+                    $arguments[] = $key . '=' . $value;
                 }
             }
-            // We always have to filter the ingestion date (published) with the las tile we 
-            // dispatched the alert    
+            // We always have to filter the ingestion date (published) with the las tile we
+            // dispatched the alert
             $arguments[] = 'startPublishedDate=' . date("Y-m-d\TH:i:s", strtotime($row["last_dispatch"]));
             // We add the arguments to the url
-             return $url . '?' . join('&', $arguments);
+            return $url . '?' . join('&', $arguments);
         } else {
-            // If we want the products ingested into resto from the last alert dispatch we need 
-            // to filter the "published" column using last_dispatch 
+            // If we want the products ingested into resto from the last alert dispatch we need
+            // to filter the "published" column using last_dispatch
             return 'http://localhost/resto/api/collections/search.json?startPublishedDate=' . $row["last_dispatch"];
         }
     }
@@ -447,7 +480,7 @@ class Alerts extends RestoModule {
      *
      * @param array $url Request url
      */
-    private function openSearchRequest($url) { 
+    private function openSearchRequest($url) {
         // Call Resto to get
         $curl = curl_init();
         curl_setopt_array($curl, array (
@@ -461,15 +494,14 @@ class Alerts extends RestoModule {
     
     /**
      * Create the message body for the mail
-     * 
+     *
      * @param array $row Element returned from the database
      */
-    private function setMailMessage($row){
-        $body.= "Peps Alert\n";
-        $body.= "Title: " . $row['title'] . "\n";
-        $body.= "Creation time: " . $row['creation_time'] . "\n";
-        $body.= "Criterias: " . $row['criterias'] . "\n";
+    private function setMailMessage($row) {
+        $body .= "Peps Alert\n";
+        $body .= "Title: " . $row['title'] . "\n";
+        $body .= "Creation time: " . $row['creation_time'] . "\n";
+        $body .= "Criterias: " . $row['criterias'] . "\n";
         return $body;
     }
-    
 }

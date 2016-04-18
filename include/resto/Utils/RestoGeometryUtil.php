b5e9ab22d91ef3c2b653e5bcda31551499a33d70
delmaale <driss.elmaalem@atos.net>
2016-04-18 15:04:34 +0200

    Fix issue - wkt polygon to array

--- include/resto/Utils/RestoGeometryUtil.php
+++ include/resto/Utils/RestoGeometryUtil.php
@@ -255,7 +255,7 @@ class RestoGeometryUtil {
          * Patterns
         */
         $lon = $lat = '[-]?[0-9]{1,3}\.?[0-9]*';
-        $values = "($lon $lat)(\s*,\s*$lon $lat)*";
+        $values = "($lon\s+$lat)(\s*,\s*$lon\s+$lat)*";
         $pattern = "/^POLYGON\s*\(\s*\(\s*($values)\s*\)\s*\)$/i";
         /*
          * Checks input parameter (WKT String)
@@ -271,7 +271,8 @@ class RestoGeometryUtil {
                  * For each coordinate, stores lon/lat
                 */
                 for($i = 0; $i < count($coordinates); $i++) {
-                    $coordinates[$i] = explode(' ', $coordinates[$i]);
+                    preg_match_all("/$lon/", $coordinates[$i], $coord_matches);
+                    $coordinates[$i] = $coord_matches[0];
                 }
             }
         } else {

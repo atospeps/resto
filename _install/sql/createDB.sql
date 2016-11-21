a9c16760b36c442a822a9f6a4930089444a8ef66
delmaale <driss.elmaalem@atos.net>
2016-11-21 10:03:25 +0100

    FT-405 : Updating database installation script

--- _install/sql/createDB.sql
+++ _install/sql/createDB.sql
@@ -16,6 +16,24 @@ RETURNS text AS \$\$
 SELECT replace(replace(lower(unaccent(\$1)),' ','-'), '''', '-')
 \$\$ LANGUAGE sql;
 
+--
+-- PostgreSQL count estimate : https://wiki.postgresql.org/wiki/Count_estimate
+--
+CREATE FUNCTION count_estimate(query text) RETURNS INTEGER AS
+\$func\$
+DECLARE
+    rec   record;
+    ROWS  INTEGER;
+BEGIN
+    FOR rec IN EXECUTE 'EXPLAIN ' || query LOOP
+        ROWS := SUBSTRING(rec."QUERY PLAN" FROM ' rows=([[:digit:]]+)');
+        EXIT WHEN ROWS IS NOT NULL;
+    END LOOP;
+ 
+    RETURN ROWS;
+END
+\$func\$ LANGUAGE plpgsql;
+
 -- 
 -- resto schema contains collections descriptions tables
 --
@@ -145,6 +163,7 @@ CREATE TABLE resto.features (
     hashes              TEXT[],
     snowcover           NUMERIC,
     cloudcover          NUMERIC,
+    orbitdirection 		TEXT,
     new_version         TEXT,
     isnrt               INTEGER DEFAULT 0,
     realtime            TEXT,

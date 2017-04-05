d46b573a61f3d422198a048c54b8489b29ade3ad
delmaale <driss.elmaalem@atos.net>
2017-04-05 12:29:11 +0200

    PEPS-FT-376 (FA-Acquisition) requêtes SQL systématiques non optimisées

--- _migration/Script_Migration_2.4_To_2.4.1.sql
+++ _migration/Script_Migration_2.4_To_2.4.1.sql
@@ -83,26 +83,23 @@ ALTER TABLE _s3.features ALTER COLUMN cyclenumber TYPE integer USING (cyclenumbe
 
 -- ajout de colonnes pour la gestion des produits S3
 
-CREATE OR REPLACE function f_add_col(
-   _tbl regclass, _col  text, _type regtype)
-    LANGUAGE plpgsql AS
+CREATE OR REPLACE function f_add_col(_tbl regclass, _col  text, _type regtype)
+  RETURNS bool AS
 $func$
 BEGIN
-
-IF EXISTS (
-   SELECT 1 FROM pg_attribute
-   WHERE  attrelid = _tbl
-   AND    attname = _col
-   AND    NOT attisdropped) THEN
-   RETURN FALSE;
-
-ELSE
-	EXECUTE 'ALTER TABLE ' || _tbl || ' ADD COLUMN ' || _col || ' ' || _type;
-   	RETURN TRUE;	
-END IF;
-
+   IF EXISTS (SELECT 1 FROM pg_attribute
+              WHERE  attrelid = _tbl
+              AND    attname = _col
+              AND    NOT attisdropped) THEN
+      RETURN FALSE;
+   ELSE
+      EXECUTE 'ALTER TABLE ' || _tbl || ' ADD COLUMN ' || _col || ' ' || _type;
+      RETURN TRUE;
+   END IF;
 END
-$func$;
+$func$  LANGUAGE plpgsql;
+
+
 
 SELECT f_add_col('_s3.features', 'approxSize', 'TEXT');
 SELECT f_add_col('_s3.features', 'ecmwfType', 'TEXT');
@@ -113,3 +110,94 @@ SELECT f_add_col('_s3.features', 'onlineQualityCheck', 'TEXT');
 -- PEPS-FT-334
 --
 SELECT f_add_col('resto.features', 'checksum', 'TEXT');
+
+--
+-- PEPS-FT-376 - Optimisation Postgresql
+--
+
+--
+-- Search index on property published
+-- ---------------------------------------------------------------------------------
+DROP INDEX IF EXISTS resto.resto_features_published_idx;
+CREATE INDEX resto_features_published_idx ON resto.features (published DESC);
+
+DROP INDEX IF EXISTS _s1._s1_features_published_idx;
+CREATE INDEX _s1_features_published_idx ON _s1.features (published DESC);
+
+DROP INDEX IF EXISTS _s2._s2_features_published_idx;
+CREATE INDEX _s2_features_published_idx ON _s2.features (published DESC);
+
+DROP INDEX IF EXISTS _s2st._s2st_features_published_idx;
+CREATE INDEX _s2st_features_published_idx ON _s2st.features (published DESC);
+
+DROP INDEX IF EXISTS _s3._s3_features_published_idx;
+CREATE INDEX _s3_features_published_idx ON _s3.features (published DESC);
+
+--
+-- Search index on property sensormode
+-- ---------------------------------------------------------------------------------
+DROP INDEX IF EXISTS resto.resto_features_sensormode_idx;
+CREATE INDEX resto_features_sensormode_idx ON resto.features (sensormode DESC);
+
+DROP INDEX IF EXISTS _s1._s1_features_sensormode_idx;
+CREATE INDEX _s1_features_sensormode_idx ON _s1.features (sensormode DESC);
+
+DROP INDEX IF EXISTS _s2._s2_features_sensormode_idx;
+CREATE INDEX _s2_features_sensormode_idx ON _s2.features (sensormode DESC);
+
+DROP INDEX IF EXISTS _s2st._s2st_features_sensormode_idx;
+CREATE INDEX _s2st_features_sensormode_idx ON _s2st.features (sensormode DESC);
+
+DROP INDEX IF EXISTS _s3._s3_features_sensormode_idx;
+CREATE INDEX _s3_features_sensormode_idx ON _s3.features (sensormode DESC);
+
+--
+-- Indexes for requests which manages obsolescence
+-- ---------------------------------------------------------------------------------
+
+--
+-- dhusingestdate
+--
+DROP INDEX IF EXISTS resto.resto_features_dhusingestdate_idx;
+CREATE INDEX resto_features_dhusingestdate_idx ON resto.features (dhusingestdate DESC);
+
+DROP INDEX IF EXISTS _s1._s1_features_dhusingestdate_idx;
+CREATE INDEX _s1_features_dhusingestdate_idx ON _s1.features (dhusingestdate DESC);
+
+DROP INDEX IF EXISTS _s2._s2_features_dhusingestdate_idx;
+CREATE INDEX _s2_features_dhusingestdate_idx ON _s2.features (dhusingestdate DESC);
+
+DROP INDEX IF EXISTS _s2st._s2st_features_dhusingestdate_idx;
+CREATE INDEX _s2st_features_dhusingestdate_idx ON _s2st.features (dhusingestdate DESC);
+
+DROP INDEX IF EXISTS _s3._s3_features_dhusingestdate_idx;
+CREATE INDEX _s3_features_dhusingestdate_idx ON _s3.features (dhusingestdate DESC);
+
+-- productidentifier
+-- 
+DROP INDEX IF EXISTS resto.resto_features_productidentifier_idx;
+CREATE INDEX resto_features_productidentifier_idx ON resto.features (productidentifier DESC);
+
+DROP INDEX IF EXISTS _s1._s1_features_productidentifier_idx;
+CREATE INDEX _s1_features_productidentifier_idx ON _s1.features (productidentifier DESC);
+
+DROP INDEX IF EXISTS _s2._s2_features_productidentifier_idx;
+CREATE INDEX _s2_features_productidentifier_idx ON _s2.features (productidentifier DESC);
+
+DROP INDEX IF EXISTS _s2st._s2st_features_productidentifier_idx;
+CREATE INDEX _s2st_features_productidentifier_idx ON _s2st.features (productidentifier DESC);
+
+DROP INDEX IF EXISTS _s3._s3_features_productidentifier_idx;
+CREATE INDEX _s3_features_productidentifier_idx ON _s3.features (productidentifier DESC);
+
+--
+-- Admin module
+-- ----------------------------------------------------------------------------------
+
+--
+-- querytime
+--
+DROP INDEX IF EXISTS usermanagement.history_querytime_idx;
+CREATE INDEX history_querytime_idx ON usermanagement.history (querytime DESC);
+
+

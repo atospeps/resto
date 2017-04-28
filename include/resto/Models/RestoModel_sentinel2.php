abf6c0ce57be96b6be4ec272afd3f2dd111efacc
delmaale <driss.elmaalem@atos.net>
2017-04-28 10:01:10 +0200

    PEPS-FT-334 : checksum MD5

--- include/resto/Models/RestoModel_sentinel2.php
+++ include/resto/Models/RestoModel_sentinel2.php
@@ -149,6 +149,7 @@ class RestoModel_sentinel2 extends RestoModel {
                     'productIdentifier' => $this->getElementByName($dom, 'title'),
                     'title' => $this->getElementByName($dom, 'title'),
                     'resourceSize' => $this->getElementByName($dom, 'resourceSize'),
+                    'resourceChecksum' => $this->getElementByName($dom, 'checksum'),
                     'authority' => 'ESA',
                     'startDate' => $this->getElementByName($dom, 'startTime'),
                     'completionDate' => $this->getElementByName($dom, 'stopTime'),

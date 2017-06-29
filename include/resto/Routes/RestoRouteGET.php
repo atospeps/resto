b814f8e7b6a0f17fadc1bd3a233dbd23d4716a2a
fpriola <franck.priola.external@atos.net>
2017-06-29 11:04:03 +0200

    FT-503: new keywords for processingLevel and s2st + fix RestoRoute.php / $this->collection => $collection

--- include/resto/Routes/RestoRouteGET.php
+++ include/resto/Routes/RestoRouteGET.php
@@ -702,7 +702,7 @@ class RestoRouteGET extends RestoRoute {
          * Not downloadable
         */
         if (!isset($featureProp['properties']['services']) || !isset($featureProp['properties']['services']['download']))  {
-            $this->user->storeQuery('ERROR', 'download', $this->collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
+            $this->user->storeQuery('ERROR', 'download', $collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
             RestoLogUtil::httpError(404);
         }
     
@@ -713,7 +713,7 @@ class RestoRouteGET extends RestoRoute {
             $filePath = $featureProp['properties']['resourceInfos']['path'];
     
             if ( !file_exists($filePath) || ($handle = fopen($filePath, "rb"))===false ) {
-                $this->user->storeQuery('ERROR', 'download', $this->collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
+                $this->user->storeQuery('ERROR', 'download', $collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
                 RestoLogUtil::httpError(404);
             }
     
@@ -726,7 +726,7 @@ class RestoRouteGET extends RestoRoute {
         else if (isset($featureProp['properties']['services']['download']['url']) && RestoUtil::isUrl($featureProp['properties']['services']['download']['url'])) {
             $filePath = $featureProp['properties']['services']['download']['url'];
             if ( ($fp = fopen($filePath, "rb"))===false ) {
-                $this->user->storeQuery('ERROR', 'download', $this->collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
+                $this->user->storeQuery('ERROR', 'download', $collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
                 RestoLogUtil::httpError(404);
             }
             fclose($fp);
@@ -742,7 +742,7 @@ class RestoRouteGET extends RestoRoute {
         * User do not have right to download product
         */
         if (!$this->user->canDownload($collection->name, $feature->identifier)) {
-        $this->user->storeQuery('ERROR', 'download', $this->collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
+        $this->user->storeQuery('ERROR', 'download', $collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
         RestoLogUtil::httpError(403);
         }
     
@@ -767,7 +767,7 @@ class RestoRouteGET extends RestoRoute {
          * Or the user has reached his instant download limit
          */
         if ($size > $this->user->profile['instantdownloadvolume'] * 1048576) {
-            $this->user->storeQuery('ERROR', 'download', $this->collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
+            $this->user->storeQuery('ERROR', 'download', $collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
             return RestoLogUtil::httpError(420, "instant|" . $this->user->profile['instantdownloadvolume']);
         }
         /*
@@ -777,10 +777,10 @@ class RestoRouteGET extends RestoRoute {
                 'userprofile' => $this->user->profile,
                 'size' => $size
         ))) {
-            $this->user->storeQuery('ERROR', 'download', $this->collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
+            $this->user->storeQuery('ERROR', 'download', $collection->name, $featureProp['id'], $this->context->query, $this->context->getUrl());
             return RestoLogUtil::httpError(420, "week|" . $this->user->profile['weeklydownloadvolume']);
         
-        }else {
+        } else {
             return "OK";
         }
     }

90d9e8eeed9f19285ca1fb3d75b9d6b784f64c2d
delmaale <driss.elmaalem@atos.net>
2017-07-04 23:43:54 +0200

    TRAITEMENTS - WSDL

--- lib/WPS/WSDL.php
+++ lib/WPS/WSDL.php
@@ -1,6 +1,9 @@
 <?php
 class WSDL {
-    
+
+    const PATTERN_WSDL_OPERATION = '/^ExecuteProcess(Async)?_(((?!(Request)|(Response)).*)*)$/';
+    const PATTERN_WSDL_MESSAGE = '/^ExecuteProcess(Async)?_(((?!Response|Request).)+)(Response|Request)?$/';
+
     /**
      *
      * TODO
@@ -14,66 +17,123 @@ class WSDL {
 
         $data = array();
         $url = $url . (substr($url, -1) == '?' ? '' : '?') . WPS_RequestManager::WSDL;
+
+        $full_wps_rights = in_array('all', $processes_enabled);
+        $response = Curl::Get($url, $data, $options);
+
+        if ($full_wps_rights == false) 
+        {
+
+            $dom = new DOMDocument;
+            // pretty print options
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = true;
         
+            $dom->loadXML($response);
         
-        /*
-         * Filter
-         */
-        // Is allowed to perform all processes ?
-//         $full_wps_rights = in_array('all', $processes_enabled);
-        
-//         // Getting processes list to perform (from query)
-//         $processes = explode(',', $data['identifier']);
-        
-//         // ? Is not allowed to perform all processes
-//         if ($full_wps_rights == false) {
+            $itemsToRemove = array();
             
-//                 // Getting wps response
-//                 $response = Curl::Get($url, $data, $options);
-                
-//                 $dom = new DOMDocument();
-//                 // pretty print options
-//                 $dom->preserveWhiteSpace = false;
-//                 $dom->formatOutput = true;
-                
-//                 $dom->loadXML($response);
-                
-//                 $processes = $dom->getElementsByTagName('ProcessDescription');
-//                 $processesToRemove = array ();
-                
-//                 if ($processes && $processes->length > 0) {
-//                     // Removing not allowed processes
-//                     foreach ($processes as $process) {
-//                         $identifier = $process->getElementsByTagNameNS('http://www.opengis.net/ows/1.1', 'Identifier');
-//                         if ($identifier && $identifier->length > 0) {
-//                             if (!in_array($identifier->item(0)->nodeValue, $processes_enabled)) {
-//                                 $processesToRemove[] = $process;
-//                             }
-//                         }
-//                     }
-//                     foreach ($processesToRemove as $process) {
-//                         $process->parentNode->removeChild($process);
-//                     }
-//                     // Updating WPS response
-//                     $response = $dom->saveXML();
-//                 }
-//                 return $response;
-//         }
+            // Removes operations from xml response
+            $operations = $dom->getElementsByTagNameNS('http://schemas.xmlsoap.org/wsdl/', 'operation');
+
+            if ($operations && $operations->length > 0) 
+            {
+                foreach ($operations as $operation) 
+                {                    
+                    $name = $operation->getAttribute('name');
+                    
+                    if (preg_match(self::PATTERN_WSDL_OPERATION, $name, $matches) 
+                            && count($matches) === 4) 
+                    {
+                        $identifier = $matches[2];
+                        if (!in_array($identifier, $processes_enabled)) 
+                        {
+                            $itemsToRemove[] = $operation;
+                        }
+                    }
+                    else 
+                    {
+                        $itemsToRemove[] = $operation;
+                    }
+                }
+            }
+            
+            // Removes message from xml response
+            $messages = $dom->getElementsByTagNameNS('http://schemas.xmlsoap.org/wsdl/', 'message');
+            
+            if ($messages && $messages->length > 0)
+            {
+                foreach ($messages as $message)
+                {
+                    $name = $message->getAttribute('name');
+            
+                    if (preg_match(self::PATTERN_WSDL_MESSAGE, $name, $matches)
+                            && (count($matches) === 5 || count($matches) === 4))
+                    {
+                        $identifier = $matches[2];
+                        if (!in_array($identifier, $processes_enabled))
+                        {
+                            $itemsToRemove[] = $message;
+                        }
+                    }
+                    else
+                    {
+                        // Not remove 'ExceptionResponse'
+                        if ($name !== 'ExceptionResponse')
+                        {
+                            $itemsToRemove[] = $message;
+                        }
+                    }
+                }
+            }
+            
+            ////////////////////
+            
+            // Removes schemas from xml response
+            $schemas = $dom->getElementsByTagNameNS('http://www.w3.org/2001/XMLSchema', 'schema');
+            
+            if ($schemas && $schemas->length > 0)
+            {
+                foreach ($schemas as $schema)
+                {
+                    $element = $schema->getElementsByTagName('element');
+                    if ($element && $element->length > 0)
+                    {
+                        
+                        $name = $element->item(0)->getAttribute('name');
+                        error_log($name, 0);
+                        if (preg_match(self::PATTERN_WSDL_MESSAGE, $name, $matches)
+                                && (count($matches) === 5 || count($matches) === 4))
+                        {
+                            $identifier = $matches[2];
+                            if (!in_array($identifier, $processes_enabled))
+                            {
+                                $itemsToRemove[] = $schema;
+                            }
+                        }
+                        else
+                        {
+                            // Not remove 'ExceptionResponse'
+                            if ($name !== 'ExceptionReport')
+                            {
+                                $itemsToRemove[] = $schema;
+                            }
+                        }
+                    }            
+                }
+            }
+            
+            ///////////////////
+            
+            // updates xml response
+            foreach($itemsToRemove as $item)
+            {
+                $item->parentNode->removeChild($item);
+            }
+            $response = $dom->saveXML();            
+        }
+        return $response;
         
-        /*
-         * Forward
-         */
-        return Curl::Get($url, $data, $options);
     }
     
-    /**
-     *
-     * @param unknown $url
-     * @param unknown $data
-     * @param unknown $processes_enabled
-     * @param unknown $options
-     */
-    public static function Post($url, $data, $processes_enabled, $options) {
-        return Curl::Post($url, $data, $options);
-    }
 }

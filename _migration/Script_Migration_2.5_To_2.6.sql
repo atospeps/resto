f8bf9fb00e6cee5bfdd2bc30553ebe4ea467be3f
fpriola <franck.priola.external@atos.net>
2017-09-20 17:25:52 +0200

    correctif anomalie rights.wps

--- _migration/Script_Migration_2.5_To_2.6.sql
+++ _migration/Script_Migration_2.5_To_2.6.sql
@@ -47,6 +47,26 @@ BEGIN
 END
 $func$  LANGUAGE plpgsql;
 
+--
+-- suppression d'une colonne
+--
+CREATE OR REPLACE function f_drop_col(_tbl regclass, _col  text)
+  RETURNS bool AS
+$func$
+BEGIN
+      EXECUTE 'ALTER TABLE ' || _tbl || ' DROP COLUMN IF EXISTS ' || _col;
+      RETURN TRUE;
+END
+$func$  LANGUAGE plpgsql;
+
+
+-- ---------------------------------------------------------------------------------
+--
+-- correctif anomalie rights.wps
+--
+-- ---------------------------------------------------------------------------------
+
+SELECT f_drop_col('usermanagement.rights', 'wps');
 
 -- ---------------------------------------------------------------------------------
 --

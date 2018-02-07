e1220d4f5e4ee8e0d7f2c743eeb6429d0a351f68
fpriola <franck.priola.external@atos.net>
2018-02-07 11:45:55 +0100

    VIZO CHECK : sauvegarde intermédiaire

--- _scripts/vizoCheck.sh
+++ _scripts/vizoCheck.sh
@@ -2,7 +2,8 @@
 
 ##############################################################
 #
-# METS A JOURS LE STATUS DE L'INFRA VIZO
+# METS A JOURS LA BASE AVEC LE STATUS DE L'INFRA VIZO
+# (ET RETOURNE LE DERNIER STATUS [option -s])
 #
 #   Fait appel à RESTo qui exécutera le traitement CHECK et
 #   mettra à jour la table wps_status
@@ -17,17 +18,45 @@ PRG="$0"
 EXEC_DIR=`dirname ${PRG}`
 export PRG_DIR=`(cd ${EXEC_DIR} ; echo $PWD)`
 
-HTTPS=0
-RESTO_CURL_PROXY=0
-RESTO_DB_HOST="localhost"
-RESTO_DB_NAME=resto
+CURL_NOPROXY=0
+HOST="localhost"
 
 #####################################
 # Usage
 #####################################
 usage() { 
-	echo "Usage: $0" 1>&2; 
+	echo "Usage: $0 -u <username:password> [-H <host> (default localhost) -n (use --noproxy curl options)]" 1>&2; 
 	exit 1; 
 }
 
+#####################################
+# OPTIONS
+#####################################
+while getopts "snu:H:" options; do
+    case $options in
+        u ) AUTH=`echo $OPTARG`;;
+        H ) HOST=`echo $OPTARG`;;
+        n) CURL_NOPROXY=1 ;;
+        s ) RETURN_STATUS=1;;
+        \? ) echo -e $usage
+            exit 1;;
+        * ) echo -e $usage
+            exit 1;;
+    esac
+done
+
+if [ -z "${AUTH}" ] || [ -z "${HOST}" ];
+then
+    usage
+fi
+
+#####################################
+# CALL RESTO
+#####################################
+if [ "$CURL_NOPROXY" = "1" ]
+then
+    curl -s --noproxy "$HOST" -X PUT https://$AUTH@$HOST/resto/wps/check
+else
+    curl -s -X PUT https://$AUTH@$HOST/resto/wps/check
+fi
 

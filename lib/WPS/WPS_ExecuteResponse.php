9b0240235b68bf64a0afe2999f9e09cd6da1c093
delmaale <driss.elmaalem@atos.net>
2018-04-13 16:31:29 +0200

    Fix issue (robustesse)

--- lib/WPS/WPS_ExecuteResponse.php
+++ lib/WPS/WPS_ExecuteResponse.php
@@ -326,7 +326,11 @@ class WPS_ExecuteResponse extends WPS_Response {
             if (count($this->processOutputs)> 0) {
                 $result = $this->processOutputs[0];
                 if (strtolower($result['identifier']) == 'report') {
-                    return json_decode($result['value'], true);
+                    $response = json_decode($result['value'], true);                    
+
+                    if (!empty($response['job_status'])) {
+                        return $response;
+                    }
                 }
             }
         } catch (Exception $e) {}

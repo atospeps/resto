cc07c3a1a48334128571ba4d1e0de2a8718c4137
delmaale <driss.elmaalem@atos.net>
2019-05-14 13:26:02 +0200

    PEPS-FT-821 : Mode de stockage incorrect dans l'historique des commandes

--- include/resto/RestoOrder.php
+++ include/resto/RestoOrder.php
@@ -38,6 +38,15 @@ class RestoOrder{
      */
     private $order = array();
     
+    /*
+     * Storage mode constants
+     */
+    const STORAGE_MODE_DISK = 'disk';
+    const STORAGE_MODE_STAGING = 'staging';
+    const STORAGE_MODE_TAPE = 'tape';
+    const STORAGE_MODE_UNAIVALABLE = 'unaivalable';
+    const STORAGE_MODE_UNKNOWN = 'unknown';
+    
     /**
      * Constructor
      * 
@@ -57,10 +66,33 @@ class RestoOrder{
         $userOrder = $this->context->dbDriver->get(RestoDatabaseDriver::ORDERS, array('email' => $this->user->profile['email'], 'orderId' => $orderId));
     
         $features = array();
+        $postData = array();
+        $storageInfos = array();
+        
         foreach ($userOrder['items'] as $item) {
-            $features[] = $this->context->dbDriver->get(RestoDatabaseDriver::FEATURE_DESCRIPTION, array('context' => $this->context, 'user' => $this->user, 'featureIdentifier' => $item['id']));
+            $feature = $this->context->dbDriver->get(RestoDatabaseDriver::FEATURE_DESCRIPTION, array('context' => $this->context, 'user' => $this->user, 'featureIdentifier' => $item['id']));
+            $features[] = $feature;
+            // If NRT, stoage mode is disk
+            if (isset($feature['properties']['isNrt']) && $feature['properties']['isNrt'] == 1){
+                $storageInfos[$feature['properties']['title']] = array('storage' => self::STORAGE_MODE_DISK);
+                continue;
+            }
+            if (isset($feature['properties']['hpssResource'])) {
+                $postData[] = $feature['properties']['hpssResource'];
+            }
         }
-
+        
+        if (!empty($postData)) {
+            $postData = $this->getStorageInfo($postData);
+        }
+        $storageInfos = array_merge($storageInfos, $postData);
+        
+        for ($i = 0, $l = count($features); $i < $l; $i++) {
+            $name = $features[$i]['properties']['title'];
+            $features[$i]['properties']['storage'] = array('mode' => isset($storageInfos[$name]['storage'])
+                ? $storageInfos[$name]['storage'] : self::STORAGE_MODE_UNKNOWN);
+        }
+        
         $userOrder['items'] = $features;
         return $userOrder;
     }
@@ -124,6 +156,50 @@ class RestoOrder{
            
         return $meta4->toString();
         
+    }
+    
+    /**
+     * Return storage information of specified data
+     * @param array $data data
+     * @param number $timeout timeout
+     * @return array storage information of inputs data
+     */
+    private function getStorageInfo($data, $timeout=30) {
+        
+        $result = array();
+        /*
+         * Storage informations
+         */
+        if (isset($data) && !empty($this->context->hpssRestApi['getStorageInfo'])){
+            $curl = curl_init($this->context->hpssRestApi['getStorageInfo']);
+            $headers = array("Content-type: text/plain");
+            curl_setopt_array($curl, array (
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_POST => 1,
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_POSTFIELDS => implode(' ', $data),
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_TIMEOUT => $timeout,
+            ));
+            
+            // Perform request
+            $response = curl_exec($curl);
+            
+            error_log(print_r($response, true), 0);
+            
+            $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            if ($response && $httpcode === 200){
+                $result = json_decode($response, true);
+            }
+            
+            if(curl_errno($curl)){
+                $error = curl_error($curl);
+                error_log($error, 0);
+            }
+            curl_close($curl);
+        }
+        return $result;
     }
     
 }
